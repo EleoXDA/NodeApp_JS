074efc0302cc3c004451b80fa0bba3c0ec4222e1
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-17 22:33:00 +0200

    add response to write on the server

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end(); //we need to end the response; any text after this will end up in an error
 });
 
 server.listen(3000);
