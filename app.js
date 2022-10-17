df7ff628ac8c6d9f1e26af701f384fe459919554
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-17 22:27:41 +0200

    add url and method before headers in request

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const http = require('http');
 // });
 
 const server = http.createServer((req,res) => {
-  console.log(req)
+  console.log(req.url, req.method, req.headers);
 });
 
 server.listen(3000);
