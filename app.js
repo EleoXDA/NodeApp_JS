ae72800a9a6cc429dfc9e512bd41d179bd88e348
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-17 20:17:38 +0200

    import http module

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const fs = require('fs') // one of Node.JS's modules for file system functionality
+const fs = require('fs'); // one of Node.JS's modules for file system functionality
+const http = require('http');
 
 console.log('Hello from Node.JS')
 fs.writeFileSync('hello.txt', 'Hello from Node.JS'); // first part is filepath+filename, second is content
+
