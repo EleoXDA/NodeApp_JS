772349b88c8035a0c2211eb8e31338b3f85c0b0a
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-17 22:40:36 +0200

    set a different home address that open a new page

--- app.js
+++ app.js
@@ -14,7 +14,18 @@ const http = require('http');
 
 // });
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end(); //we need to write return so that the rest of the commands dont execute after res.end()
+  }
   console.log(req.url, req.method, req.headers);
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
