28827dbcf706e0544b90c6ffcffae1b9ba0fdcdb
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-18 23:51:24 +0200

    export requestHandler to a separate js file

---
+++ routes.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const requestHandler = (req,res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    // res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end(); //we need to write return so that the rest of the commands dont execute after res.end()
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.text', message, (err) => {/*we need to make it async so that other incoming events can be handled*/
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      }); 
+    });
+  }
+  // console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end(); //we need to end the response; any text after this will end up in an error
+};
+
+module.exports = requestHandler;
