97e306201aa101ec43d29b6573e9044507fc9a45
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-18 23:52:10 +0200

    import routes.js in app.js

--- app.js
+++ app.js
@@ -1,55 +1,8 @@
-const fs = require('fs');
 const http = require('http');
 
-// console.log('Hello from Node.JS')
-// fs.writeFileSync('hello.txt', 'Hello from Node.JS'); // first part is filepath+filename, second is content
-
-// function rqListener(req, res) {
-
-// }
-
-// http.createServer(rqListener);
-
-// http.createServer(function(req, res) {
-
-// });
+const routes = require('./routes.js');
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    // res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write('</html>');
-    return res.end(); //we need to write return so that the rest of the commands dont execute after res.end()
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.text', message, (err) => {/*we need to make it async so that other incoming events can be handled*/
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      }); 
-    });
-  }
-  // console.log(req.url, req.method, req.headers);
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end(); //we need to end the response; any text after this will end up in an error
 });
 
 server.listen(3000);
